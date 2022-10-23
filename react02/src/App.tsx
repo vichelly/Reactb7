e7225511302eb6cc155a5e886d82d681ee0c9a68
Vitor Lucas Fujita Felício <vlucasff@hotmail.com>
2022-10-22 22:49:19 -0300

    att

--- react02/src/App.tsx
+++ react02/src/App.tsx
@@ -1,13 +1,40 @@
+import React,{useState} from 'react';
+
 const App = () => {
 
-  /* const buttonclick = () => {
-    alert("Você clicou");
-  } */
+  const [name, setName] = useState('Vitor Lucas');
+
+  const buttonclick = () => {
+    setName('Vichelly');
+  };
+
+  const [n,setN] = useState(0);
+
+  const handleminus = () => {
+    setN(n-1);
+  }
+  const handleplus = () => {
+    setN(n+1);
+  }
+
+  const[nome, setNome] = useState('');
+
+  const handleinput = (event: React.ChangeEvent<HTMLInputElement> ) => {
+    setNome(event.target.value);
+  }
 
   return(
     <div>
-      <button onClick={()=>{alert("You clicked")}} 
-      /* onClick={buttonclick} */ > Click Here </button>
+      Nome: <input type="text" value={nome} onChange={handleinput} />
+      <h2>seu nome é {nome}</h2>
+      <hr />
+      <h1>Contador</h1>
+      <button onClick={handleminus} >-</button>
+      <h2>{n}</h2>
+      <button onClick={handleplus} >+</button>
+      <hr />
+      o número é {name}
+      <button onClick={buttonclick} > Click Here </button>
     </div>
   );
 }
