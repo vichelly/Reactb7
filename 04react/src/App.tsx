c8621ac76871aa16ddef983b2f5d4a39be06c324
Vitor <vlucasff@hotmail.com>
2022-10-31 15:43:08 -0300

    att

--- 04react/src/App.tsx
+++ 04react/src/App.tsx
@@ -1,5 +1,9 @@
 //importa o componente de components
 import {Botao} from './components/buton';
+import {Pessoa} from './components/Pessoa';
+// reenderizando conteúdo condicional
+import { useState } from 'react';
+
 
 function App() {
 
@@ -7,9 +11,59 @@ function App() {
     alert("Frase do app: "+txt);
   }
 
+  let list =[
+    'Maneiro','Legal','Bacana','Show','Massa'
+  ];
+  //função map: executa algo em cada item do array
+
+  let pessoa = [
+    {name: 'Genilson', age: 45 },
+    {name: 'Rosvald', age: 32 },
+    {name: 'Gerald', age: 56 },
+    {name: 'Gislene', age: 34}
+  ]
+
+  /* constante da condicional */
+  const [show, setShow] = useState(false);
+  /* fução para mostrar o condicional com toggle */
+  const handleClick = () => {
+    /* if(show){
+      setShow(false);
+    }
+    else{
+      setShow(true);
+    }    
+    
+    é possível fazer o mesmo de uma maneira mais facil
+    com apenas uma linha!
+    */
+
+    setShow(!show);
+
+  }
+
   return (
     <div>
-      <Botao text="clique aqui" clickfn={botaoEvent} ></Botao>
+      <Botao text="clique aqui" clickfn={botaoEvent} ></Botao><br />
+      <h2>Lista de adjetivos</h2>
+      <ul>
+        {list.map((item, index )=>(
+          //colocar o key para ordenar, index = cada um tem um
+          <li key={index} >{item}</li>
+        ))}
+        <br />
+        {pessoa.map((item, index)=>(
+          <Pessoa data={item}/>
+        ) )}
+        <br />
+        <br />
+
+
+        {/* reenderizar conteúdo condicional */}
+        <button onClick={handleClick} >{show ? 'Ocultar' : 'Mostrar'}</button>
+
+        {show === true && <div>Jacksons Five</div>}
+      </ul>
     </div>
   )
 }
