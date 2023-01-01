1762e82399262b35b18b51ae67e54beff6b192cc
Vitor <vlucasff@hotmail.com>
2023-01-01 17:02:49 -0300

    att

--- 02react/src/App.tsx
+++ 02react/src/App.tsx
@@ -2,50 +2,25 @@ import React,{useState} from 'react';
 
 const App = () => {
 
-  // uso de states
+  const [numero, setNumero] = useState(1);
 
- /*  const [name, setName] = useState('Vitor Lucas');
+  const [mult, setMult] = useState(32);
 
-  const buttonclick = () => {
-    setName('Vichelly');
-  };
- */
-
-  //criando o contador usando state
-  const [n,setN] = useState(0);
-
-  const handleminus = () => {
-    setN(n-1);
-  }
-  const handleplus = () => {
-    setN(n+1);
+  const More = () => {
+    setNumero(numero + 1)
+    setMult(mult * 32)
+  } 
+  const Minus = () => {
+    setNumero(numero - 1)
+    setMult(mult / 32 )
   }
 
-  
-  // criando o input que atualiza
-  const[nome, setNome] = useState('');
-
-  const handleinput = (event: React.ChangeEvent<HTMLInputElement> ) => {
-    setNome(event.target.value);
-  }
 
   return(
     <div>
-      <hr />
-      <h2>Contador</h2>
-      <button onClick={handleminus} >-</button>
-      <h1>{n}</h1>
-      <button onClick={handleplus} >+</button>
-      <hr />
-      <br /><br />
-      <hr />
-      <h2>atualiza em tempo real</h2>
-      <h2>Digite seu nome</h2> <input type="text" value={nome} onChange={handleinput} />
-      <h2>seu nome é {nome}</h2>
-      <hr />
-      <br /><br />
-     {/*  o número é {name}
-      <button onClick={buttonclick} > Click Here </button> */}
+      <button onClick={More}  >+</button>
+      <h1>Tabuada do 32: 32 x {numero} = {mult} </h1>
+      <button onClick={Minus}  >-</button>
     </div>
   );
 }
